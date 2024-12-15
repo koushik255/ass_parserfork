ea9c8d963f5473e70100796f998d626b5f366dcd
Aavtic <aadishm79@gmail.com>
2024-12-15 13:37:45 +0530

    Updated Documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -156,6 +156,31 @@
 ///     Ok(())
 /// }
 /// ```
+/// # Read Contents of ASS Files
+/// 
+/// To retrieve the values of fields present in each dialogue. You can load a `.ass` file and then access each dialogue details. 
+/// 
+/// ```rust
+/// use ass_parser::{AssFile, Dialogue};
+/// 
+/// let ass_file = AssFile::from_file("examples/subtitles.ass")?;
+/// let dialogues: Vec<Dialogue> = ass_file.events.get_dialogues();
+/// 
+/// for dialogue in dialogues {
+///     println!("layer: {:?}", &dialogue.get_layer());
+///     println!("name: {:?}", &dialogue.get_name());
+///     println!("end: {:?}", &dialogue.get_end());
+///     println!("start: {:?}", &dialogue.get_start());
+///     println!("text: {:?}", &dialogue.get_text());
+///     println!("marginl: {:?}", &dialogue.get_marginl());
+///     println!("marginr: {:?}", &dialogue.get_marginr());
+///     println!("marginv: {:?}", &dialogue.get_marginv());
+///     println!("style: {:?}", &dialogue.get_style());
+///     println!("effect: {:?}", &dialogue.get_effect());
+///     println!("colour: {:?}", &dialogue.get_colour());
+/// }
+/// ```
+///
 ///
 /// # Added Support for SubRip files.
 ///
